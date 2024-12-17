a096ec5cc164452195929480ce00f7f7670a0ff4
Fairys Ardhana <ardhana.f@skalar.com>
2024-12-17 15:52:52 +0100

    features library frontend

---
+++ libs/game-review-platform/features/src/lib/company/company-details/company-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-company-details',
+  imports: [CommonModule],
+  templateUrl: './company-details.component.html',
+  styleUrl: './company-details.component.css',
+})
+export class CompanyDetailsComponent {}
---
+++ libs/game-review-platform/features/src/lib/company/company-edit/company-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-company-edit',
+  imports: [CommonModule],
+  templateUrl: './company-edit.component.html',
+  styleUrl: './company-edit.component.css',
+})
+export class CompanyEditComponent {}
